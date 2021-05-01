71a171687445f0a76de75063e501be87e40dd286
Mel <meljohnpualon@gmail.com>
2021-05-02 02:14:38 +0800

    added slick slides

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,17 @@
 // $('.carousel').carousel()
+// $('.multiple-items').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+// });
+
+$(document).ready(function(){
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+    });
+});
